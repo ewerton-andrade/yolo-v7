84373c3e5c110c4018c3f479843a3ec7039b1cd6
ewerton-senai <ewerton.andrade@sp.senai.br>
2024-09-25 10:00:45 -0300

    feat: especificação para detecção em imagens usando  o modelo pré-treinado do COCO

--- run_training.sh
+++ run_training.sh
@@ -7,6 +7,12 @@
 #Face mask detection for windows
 # python .\detect.py --weights .\runs\train\yolov7-face-mask\weights\best.pt --source 0
 
+#COCO detection for windows (camera)
+# python .\detect.py --weights .\yolov7.pt --conf-thres 0.5 --img-size 640 --source 0
+
+#COCO detection for windows (other source)
+# python .\detect.py --weights .\yolov7.pt --conf-thres 0.5 --img-size 640 --source dataset\images_path
+
 #EPI training for windows
 # python train.py --workers 0 --batch-size 4 --device 0 --data data\epi.yaml --img 640 640 --cfg cfg\training\yolov7-epi.yaml --weights yolov7_training.pt --name yolov7-epi --hyp data\hyp.scratch.custom.yaml --epochs 300
 
